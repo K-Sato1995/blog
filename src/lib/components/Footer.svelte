abdd88d65a6722776bac0448912888e6a706477d
Katsuki <Katsuki_Sato0130@icloud.com>
2022-03-07 18:46:43 +0900

    Add footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,7 @@
+<footer class="footer" />
+
+<style>
+  .footer {
+    border-top: solid 1px var(--border-color);
+  }
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,9 @@
 <script>
   import "../assets/styles/global.css";
   import Header from "$lib/components/Header.svelte";
+  import Footer from "$lib/components/Footer.svelte";
 </script>
 
 <Header />
 <slot />
+<Footer />
