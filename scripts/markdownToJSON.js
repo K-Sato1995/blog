c52de14a65def5090f42d2dcad97d2e3085df82f
Katsuki <Katsuki_Sato0130@icloud.com>
2022-03-07 16:34:26 +0900

    Use md-to-json-converter

--- scripts/markdownToJSON.js
+++ scripts/markdownToJSON.js
@@ -1,77 +1,42 @@
-import path from 'path'
-import glob from 'glob'
-import { Remarkable  } from 'remarkable';
-import * as fs from 'fs'
+import { converteMdToJSON } from 'md-to-json-converter'
 import hljs from 'highlight.js'
-import { linkify } from 'remarkable/dist/cjs/linkify.js';
-import meta from 'remarkable-meta'
+import path from 'path'
 
 const __dirname = path.resolve();
 const contentPath = path.join(__dirname, '/content')
 const outputPath = path.join(__dirname, '/src/data/posts.json')
 
-// Config
-const remarkableOptions = {
-    html: true,
-    breaks: true,
-    langPrefix: 'hljs language-', // highlight.js css expects a top-level 'hljs' class.
-    typographer:  false,
-    highlight: (str, lang) => {
-        if (lang && hljs.getLanguage(lang)) {
-          try {
-            return hljs.highlight(lang, str).value;
-          } catch (err) {
-              console.log(err)
-          }
-        }
-    
+export const remarkableOptions = {
+  html: true,
+  breaks: true,
+  langPrefix: 'hljs language-', // highlight.js css expects a top-level 'hljs' class.
+  typographer:  false,
+  highlight: (str, lang) => {
+      if (lang && hljs.getLanguage(lang)) {
         try {
-          return hljs.highlightAuto(str).value;
+          return hljs.highlight(lang, str).value;
         } catch (err) {
             console.log(err)
         }
-    
-        return ''; 
       }
-}
-
-
-// Utils
-const getMDFiles = (path) => {
-    return glob.sync(path + '/**/*.md')
-}
-const ppJSON = (obj) => JSON.stringify(obj, null, 2)
-
-
-// MD Parser
-const parser = new Remarkable(remarkableOptions).use(linkify).use(meta);
-
-const constructJSONObj = (contentPath) => {
-    const jsonObj = {
-        "data": []
+  
+      try {
+        return hljs.highlightAuto(str).value;
+      } catch (err) {
+          console.log(err)
+      }
+  
+      return ''; 
     }
-    const mdFiles = getMDFiles(contentPath)
-
-    for(let i = 0; i < mdFiles.length; i++) {
-        const mdFile = mdFiles[i]
-        const mdText = fs.readFileSync(mdFile).toString();
-        const item = {
-            body: parser.render(mdText),
-            ...parser.meta,
-        }
-        jsonObj.data.push(item) 
-    }
-    return jsonObj
 }
 
-
-// Write
-const writeJSON = (outputPath, jsonObj) => {
-    fs.writeFile(outputPath, ppJSON(jsonObj), (err) => {
-        if (err) throw err;
-        console.log('Successfully Created the JSON FILE!');
-    });    
-}
-
-const jsonObj = constructJSONObj(contentPath)
-writeJSON(outputPath, jsonObj)
+converteMdToJSON({
+    contentPath,
+    outputPath,
+    remarkableOptions: remarkableOptions
+  }).then((data) => {
+    console.log('data:', data);
+    console.log('Successfully wrote the data')
+  }).catch((err) => {
+    console.log('error:', err);
+  })
