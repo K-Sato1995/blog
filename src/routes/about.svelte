98b3edeb98db00681980b788ac5f723bb357a06f
Katsuki <Katsuki_Sato0130@icloud.com>
2022-03-08 14:58:46 +0900

    Add links to about page;

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -2,7 +2,37 @@
   <title>K-Sato | About</title>
 </svelte:head>
 
-<h1>About me</h1>
-<p>
-  Aspiaring web developer. Rediscovering the love for coding and building stuff.
-</p>
+<div class="about-container">
+  <h1>About me</h1>
+  <p>
+    Aspiring web developer. Rediscovering the love for coding and building
+    stuff.
+  </p>
+
+  <ul class="about-container__links">
+    <li class="about-container__links__link">
+      <a href="https://github.com/K-Sato1995">Github</a>
+    </li>
+    <li class="about-container__links__link">
+      <a href="https://qiita.com/k-penguin-sato">Qiita</a>
+    </li>
+    <li class="about-container__links__link">
+      <a href="https://dev.to/k_penguin_sato">Dev.to</a>
+    </li>
+    <li class="about-container__links__link">
+      <a href="https://zenn.dev/k_sato">Zenn</a>
+    </li>
+  </ul>
+</div>
+
+<style>
+  .about-container__links {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    list-style: none;
+  }
+  .about-container__links__link {
+    margin-right: 1rem;
+  }
+</style>
