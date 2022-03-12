e72543370c66ec2541be448afd77d7897f9b5ab3
Katsuki <Katsuki_Sato0130@icloud.com>
2022-03-12 12:26:02 +0900

    change desgin of projects

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -54,17 +54,6 @@
         <p class="projects-container__content__description">
           {project.description}
         </p>
-        <div class="projects-container__content__techstack">
-          {#each project.techStack as techstack}
-            <div class="projects-container__content__techstack__item">
-              <span
-                class={`projects-container__content__techstack__item__color ${techstack.toLowerCase()}`}
-              /><span class="projects-container__content__techstack__item__name"
-                >{techstack}</span
-              >
-            </div>
-          {/each}
-        </div>
       </div>
     {/each}
   </div>
@@ -73,53 +62,24 @@
 </div>
 
 <style>
-  .projects-container {
-    --typescript-color: #2b7489;
-    --react-color: #61dafb;
-    --svelte-color: #ff3d00;
-    --eslint-color: #4b31c3;
-    --github-action-color: #242a2f;
-    --ruby-color: #a91500;
-    --go-color: #79d4fd;
-  }
-  .typescript {
-    background-color: var(--typescript-color);
-    border: 1px solid var(--typescript-color);
-  }
-  .react {
-    background-color: var(--react-color);
-    border: 1px solid var(--react-color);
-  }
-  .svelte,
-  .sveltekit {
-    background-color: var(--svelte-color);
-    border: 1px solid var(--svelte-color);
-  }
-  .eslint {
-    background-color: var(--eslint-color);
-    border: 1px solid var(--eslint-color);
-  }
-  .githubaction {
-    background-color: var(--github-action-color);
-    border: 1px solid var(--github-action-color);
-  }
-  .ruby {
-    background-color: var(--ruby-color);
-    border: 1px solid var(--ruby-color);
-  }
-  .go {
-    background-color: var(--go-color);
-    border: 1px solid var(--go-color);
-  }
-
   .projects-container__contet-wrapper {
     margin-top: 1.5rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    flex-wrap: wrap;
   }
 
   .projects-container__content {
     border-bottom: solid 1px var(--border-color);
-    padding-bottom: 1.5rem;
     margin-top: 1.5rem;
+    width: 48%;
+  }
+
+  @media screen and (max-width: 600px) {
+    .projects-container__content {
+      width: 100%;
+    }
   }
 
   .projects-container__content__name {
@@ -129,6 +89,7 @@
 
   .projects-container__content__description {
     padding: 0.5rem 0;
+    color: var(--secondary-color);
   }
 
   .projects-container__content__techstack {
