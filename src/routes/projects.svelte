654f55abb44d5f73465ddd7ea072be0a829739c7
Katsuki <Katsuki_Sato0130@icloud.com>
2022-03-12 12:34:04 +0900

    Base design done

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -54,6 +54,17 @@
         <p class="projects-container__content__description">
           {project.description}
         </p>
+        <div class="projects-container__content__techstack">
+          {#each project.techStack as techstack}
+            <div class="projects-container__content__techstack__item">
+              <span
+                class={`projects-container__content__techstack__item__color ${techstack.toLowerCase()}`}
+              /><span class="projects-container__content__techstack__item__name"
+                >{techstack}</span
+              >
+            </div>
+          {/each}
+        </div>
       </div>
     {/each}
   </div>
@@ -62,6 +73,45 @@
 </div>
 
 <style>
+  .projects-container {
+    --typescript-color: #2b7489;
+    --react-color: #61dafb;
+    --svelte-color: #ff3d00;
+    --eslint-color: #4b31c3;
+    --github-action-color: #242a2f;
+    --ruby-color: #a91500;
+    --go-color: #79d4fd;
+  }
+  .typescript {
+    background-color: var(--typescript-color);
+    border: 1px solid var(--typescript-color);
+  }
+  .react {
+    background-color: var(--react-color);
+    border: 1px solid var(--react-color);
+  }
+  .svelte,
+  .sveltekit {
+    background-color: var(--svelte-color);
+    border: 1px solid var(--svelte-color);
+  }
+  .eslint {
+    background-color: var(--eslint-color);
+    border: 1px solid var(--eslint-color);
+  }
+  .githubaction {
+    background-color: var(--github-action-color);
+    border: 1px solid var(--github-action-color);
+  }
+  .ruby {
+    background-color: var(--ruby-color);
+    border: 1px solid var(--ruby-color);
+  }
+  .go {
+    background-color: var(--go-color);
+    border: 1px solid var(--go-color);
+  }
+
   .projects-container__contet-wrapper {
     margin-top: 1.5rem;
     display: flex;
@@ -72,7 +122,8 @@
 
   .projects-container__content {
     border-bottom: solid 1px var(--border-color);
-    margin-top: 1.5rem;
+    padding-top: 1.5rem;
+    padding-bottom: 1.5rem;
     width: 48%;
   }
 
@@ -99,16 +150,17 @@
 
   .projects-container__content__techstack__item {
     margin-left: 1rem;
+    font-size: 0.8rem;
   }
   .projects-container__content__techstack__item:first-child {
     margin-left: 0;
   }
   .projects-container__content__techstack__item__color {
     position: relative;
-    top: 1px;
     display: inline-block;
-    width: 12px;
-    height: 12px;
+    top: 1px;
+    width: 10px;
+    height: 10px;
     border-radius: 50%;
   }
 
